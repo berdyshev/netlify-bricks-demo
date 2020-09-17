6ca5c8b84d58705de5692b574ca33ef945e90557
Artem Berdyshev <berdartem@gmail.com>
2020-09-17 22:30:41 +0300

    configure blocks widget for pages

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -1,4 +1,4 @@
-import htm from "https://unpkg.com/htm?module";
+import htm from 'https://unpkg.com/htm?module';
 
 const html = htm.bind(h);
 
@@ -9,12 +9,42 @@ const Page = createClass({
 
     return html`
       <main>
-        <h1>${entry.getIn(["data", "title"], null)}</h1>
+        <h1>${entry.getIn(['data', 'title'], null)}</h1>
 
-        ${this.props.widgetFor("body")}
+        ${this.props.widgetsFor('blocks').map((entry, index) => {
+          switch (entry.getIn(['data', 'type'])) {
+            case 'header':
+              return h(
+                `h${entry.getIn(['data', 'level'])}`,
+                { key: index },
+                entry.getIn(['data', 'heading'])
+              );
+
+            case 'paragraph': {
+              return h(CMS.resolveWidget('markdown').preview, {
+                key: index,
+                value: entry.getIn(['data', 'text']),
+                resolveWidget: CMS.resolveWidget,
+                getAsset: this.props.getAsset,
+              });
+            }
+
+            case 'highlight':
+              return h(CMS.resolveWidget('image').preview, {
+                key: index,
+                value: entry.getIn(['data', 'image']),
+                resolveWidget: CMS.resolveWidget,
+                getAsset: this.props.getAsset,
+              });
+
+            default:
+              return html``;
+          }
+        })}
+        ${this.props.widgetFor('body')}
       </main>
     `;
-  }
+  },
 });
 
 export default Page;
